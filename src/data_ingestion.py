5ea3ab987c373d380bbabffe25abad6f7f05d02e
Akash Kangule <akashkangule18@gmail.com>
2026-05-28 12:49:03 +0530

    this is the second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import os
 
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
-df = df.iloc[:,3:]
+df = df.iloc[:,5:]
+df = df[df['Length of Membership'] > 3]
 
 df.to_csv(os.path.join('data','customer.csv'))
